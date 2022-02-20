bba6fe3b0cb57709dcef19cc04dac32801faa50f
Patrick Morrison <patrick.morrison1498@gmail.com>
2022-02-20 18:07:17 +0800

    end of weekend

--- wrecks/forms.py
+++ wrecks/forms.py
@@ -1,7 +1,13 @@
 from .models import Report
 from django import forms
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
         fields = ['title', 'date', 'authors', 'file']
+        widgets = {
+            'date': DateInput}
+
