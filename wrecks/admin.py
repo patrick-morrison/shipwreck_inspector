326ef57c125322ac0f662d8832d0b74b2966974c
Patrick Morrison <patrick.morrison1498@gmail.com>
2022-02-20 10:23:40 +0800

    add file uploads

--- wrecks/admin.py
+++ wrecks/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Site, Report
+from .models import Site, Report, Person, Publication
 
 admin.site.register(Site)
 admin.site.register(Report)
+admin.site.register(Person)
+admin.site.register(Publication)
--- wrecks/models.py
+++ wrecks/models.py
@@ -14,11 +14,56 @@ class Site(models.Model):
     location = models.CharField(max_length=255)
     underwater = models.CharField(max_length=255)
     sinking = models.CharField(max_length=255)
-    latitude = models.DecimalField(max_digits=19, decimal_places=10)
-    longitude = models.DecimalField(max_digits=19, decimal_places=10)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name + " " + self.built + "-" + self.sunk
+    
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.RESTRICT, null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.TextField(max_length=500, null=True, blank=True)
+    active = models.BooleanField()
+    birth_date = models.DateField(null=True, blank=True)
+    POSITION_CHOICES = [
+        ('ME', 'Member'),
+        ('GU', 'Guest'),
+        ('AD', 'Admin'),
+        ('MU', 'Museum'),
+        ('OF', 'Offline'),
+    ]
+    position = models.CharField(
+        max_length=2,
+        choices= POSITION_CHOICES,
+        default='GUEST',
+    )
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 class Report(models.Model):
     title = models.CharField(max_length=255)
     date = models.DateField()
+    authors = models.ManyToManyField(Person)
     site = models.ForeignKey(Site, on_delete=models.RESTRICT)
     user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    file = models.FileField(upload_to='reports/', null=True, blank=True)
+
+    def __str__(self):
+        return self.date.strftime("%Y-%m-%d") + " " + self.site.name + " " + self.title
+
+class Publication(models.Model):
+    title = models.CharField(max_length=255)
+    authors = models.ManyToManyField(Person)
+    reports = models.ManyToManyField(Report)
+    date = models.DateField()
+    site = models.ManyToManyField(Site)
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    file = models.FileField(upload_to='publications/', null=True, blank=True)
+
+    def __str__(self):
+        return self.date.strftime("%Y-%m-%d") + " " + self.title
